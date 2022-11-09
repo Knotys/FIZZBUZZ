257fae3ff3e49a98ef8b5e7d2a857f7aa95f1f3f
Robert Knotek <knotyrk@seznam.cz>
2022-11-09 14:37:00 +0100

    Numbers between 1 to 100
    + numbers are divisible by 3 and 5

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 100;
+        System.out.println("Čísla od 1 do "+num);
+        for (int i = 1; i < num; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
